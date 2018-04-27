196a9e8799f24169d7c1b0fa1f7c331d05565a0d
shidephen <shidephen@outlook.com>
2018-04-27 14:33:12 +0800

    init

---
+++ set-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone --depth=1 https://github.com/shidephen/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
+cd ~/.vim_runtime/my_plugins/YouCompleteMe
+./install.py --clang-completer --go-completer --rust-completer
+
+wget
